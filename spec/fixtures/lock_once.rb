0b99b2d89fe2d5c1716824275ab8ac5354d760e0
schneems <richard.schneeman@gmail.com>
2015-06-05 14:26:37 -0500

    different keys for different travis jobs
    When the matrix executes it does so in parallel, so you'll get Ruby 2.2 running at the same time as 2.1 and if they all use the same keys, then they're using the same locks. This prevents that from happening.

--- spec/fixtures/lock_once.rb
+++ spec/fixtures/lock_once.rb
@@ -1,6 +1,6 @@
 require_relative 'fixture_helper.rb'
 
-PgLock.new(name: "all_your_base", attempts: 1).lock do
+PgLock.new(name: "all_your_base_#{ENV["TRAVIS_JOB_ID"]}", attempts: 1).lock do
   puts "Running locked code on: #{ Process.pid }"
   sleep 2 # hold the lock for a bit
 end
--- spec/fixtures/run_x_times.rb
+++ spec/fixtures/run_x_times.rb
@@ -1,7 +1,7 @@
 require_relative 'fixture_helper.rb'
 
 ENV["COUNT"].to_i.times do |i|
-  PgLock.new(name: "all_your_base_#{i}", attempts: 1).lock do
+  PgLock.new(name: "all_your_base_#{i}_#{ ENV["TRAVIS_JOB_ID"] }", attempts: 1).lock do
     puts "Running locked code on: #{ Process.pid }"
     sleep 2 # hold the lock for a bit
   end
