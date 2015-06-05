b12828a000388670b4192b77b35569b833f3eed4
schneems <richard.schneeman@gmail.com>
2015-06-05 14:19:56 -0500

    Slightly more deterministic test :/

--- spec/fixtures/lock_once.rb
+++ spec/fixtures/lock_once.rb
@@ -2,5 +2,5 @@ require_relative 'fixture_helper.rb'
 
 PgLock.new(name: "all_your_base", attempts: 1).lock do
   puts "Running locked code on: #{ Process.pid }"
-  sleep 1 # hold the lock for a bit
+  sleep 2 # hold the lock for a bit
 end
--- spec/fixtures/run_x_times.rb
+++ spec/fixtures/run_x_times.rb
@@ -3,6 +3,6 @@ require_relative 'fixture_helper.rb'
 ENV["COUNT"].to_i.times do |i|
   PgLock.new(name: "all_your_base_#{i}", attempts: 1).lock do
     puts "Running locked code on: #{ Process.pid }"
-    sleep 1 # hold the lock for a bit
+    sleep 2 # hold the lock for a bit
   end
 end
