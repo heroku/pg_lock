90f154a75b7f6ca0797780c4849a19c84aca67ce
schneems <richard.schneeman@gmail.com>
2015-06-10 11:20:46 -0500

    Added extra tests for most features
    Most features are now tested.

--- spec/fixtures/lock!.rb
+++ spec/fixtures/lock!.rb
@@ -1,13 +1,9 @@
 require_relative 'fixture_helper.rb'
 
-
-puts "=="
-puts ENV.fetch("PG_LOCK_KEY").inspect
+STDERR.puts "started"
 
 PgLock.new(name: ENV.fetch("PG_LOCK_KEY")).lock! do
-  puts "Running locked code on: #{ Process.pid }"
+  STDERR.puts "Running locked code on: #{ Process.pid }"
   sleep 3 # hold the lock for a bit
 end
 
-
-puts "done"
