0accdcd7453d1f0848df4ce5eceb372ca8d2ab4b
schneems <richard.schneeman+foo@gmail.com>
2019-12-03 16:22:13 -0600

    Test fails now!

--- spec/fixtures/lock!.rb
+++ spec/fixtures/lock!.rb
@@ -7,6 +7,7 @@ sleep_for = (ENV["SLEEP_FOR"] || 3).to_i
 PgLock.new(name: ENV.fetch("PG_LOCK_KEY")).lock! do
   puts "Running locked code on: #{ Process.pid }"
   sleep sleep_for # hold the lock for a bit
+  false
 end
 
 puts "done with lock!"
